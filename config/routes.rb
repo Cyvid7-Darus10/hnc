5c44ae2f213514ccc91f359c061b61ec5b8b3532
Cyrus David Pastelero <61748886+Cyvid7-Darus10@users.noreply.github.com>
2022-05-11 19:20:02 +0800

    [Layout] User Profile (#3)
    * [Layout] User Profile
    
    * updated the container class to contianer-fluid class for responsiveness
    
    * updated the application_controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  resources :users, only: [:show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
