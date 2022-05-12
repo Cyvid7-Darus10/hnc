9017b5746bf9074686f0534d843fb5533e861bb5
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-05-12 19:44:31 +0800

    [Layout] Update Room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,17 +2,38 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
-
-  def show
-  end
   
   def create
     @room = current_user.rooms.new(room_params)
     if @room.save
-      redirect_to @room
+      redirect_to listing_room_path(@room), success: "Successfully created a room"
     else
       render 'new'
     end
+  end
+
+  def listing
+    @room = Room.find(params[:id])
+  end
+
+  def price
+    @room = Room.find(params[:id])
+  end
+
+  def description
+    @room = Room.find(params[:id])
+  end
+
+  def photos
+    @room = Room.find(params[:id])
+  end
+
+  def amenities
+    @room = Room.find(params[:id])
+  end
+
+  def location
+    @room = Room.find(params[:id])
   end
 
   private
