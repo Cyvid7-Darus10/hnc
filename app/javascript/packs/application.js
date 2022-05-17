253f20143e9ae5ffd3fc9f16a90f275c19d62de8
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-05-17 21:34:25 +0800

    [Layout] Room Information

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,9 @@ require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
 
+import "bootstrap-datepicker"
+import jquery from 'jquery'
+window.$ = window.jquery = jquery
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
